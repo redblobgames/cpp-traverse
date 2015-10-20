d04a95af36345134cfb22b9cd907a547da9eb69a
Amit Patel <redblobgames@gmail.com>
2015-10-20 15:14:15 -0700

    Change recursive structure of serialization
    * I had visit() with labels call handle() without labels, which then
      called visit() with labels, recursively all the way down.
    * The labels were at the wrong level. I had to give labels at the top
      level (which doesn't need them) and also for vector elements (which
      don't need them). Only struct fields need labels.
    * I took labels out of visit() and moved them up into visit_struct().
      Anyone else calling visit() didn't need labels, so I removed those
      labels.
    * After this change, handle and visit became the same so I removed
      handle() and made the recursive structure visit->visit->visit.
    * Structs and vectors (and in the future, strings) have to be handled
      for each type of visitor. All user defined structs share one struct
      implementation (visit_struct), so the end user does NOT have to define
      something per type of visitor.

--- test.cpp
+++ test.cpp
@@ -18,21 +18,21 @@ struct Polygon {
   
 namespace traverse {
   template <typename Visitor>
-  void handle(Visitor& visitor, Point& value) {
+  void visit(Visitor& visitor, Point& value) {
     visit_struct(visitor)
       ("x", value.x)
       ("y", value.y);
   }
 
   template <typename Visitor>
-  void handle(Visitor& visitor, LineSegment& value) {
+  void visit(Visitor& visitor, LineSegment& value) {
     visit_struct(visitor)
       ("a", value.a)
       ("b", value.b);
   }
 
   template <typename Visitor>
-  void handle(Visitor& visitor, Polygon& value) {
+  void visit(Visitor& visitor, Polygon& value) {
     visit_struct(visitor)
       ("points", value.points);
   }
@@ -45,12 +45,12 @@ int main() {
   Polygon polygon = {{{3, 5}, {4, 6}, {5, 7}}};
   
   std::cout << "original data: " << std::endl;
-  visit(writer, "polygon", polygon);
+  visit(writer, polygon);
   std::cout << std::endl;
 
   std::cout << "serialized to bytes: " << std::endl;
   traverse::BinarySerialize writer2;
-  visit(writer2, "polygon", polygon);
+  visit(writer2, polygon);
   std::string msg = writer2.out.str();
   for (int i = 0; i < msg.size(); i++) {
     std::cout << int(msg[i]) << ' ';
@@ -60,8 +60,8 @@ int main() {
   std::cout << "read back from bytes: " << std::endl;
   traverse::BinaryDeserialize reader(msg);
   Polygon polygon2;
-  visit(reader, "dummy", polygon2);
-  visit(writer, "polygon", polygon2);
+  visit(reader, polygon2);
+  visit(writer, polygon2);
   std::cout << std::endl;
 
   std::cout << "corrupting data: " << std::endl;
@@ -69,7 +69,7 @@ int main() {
   msg[3] = 0x7f;
   traverse::BinaryDeserialize reader2(msg);
   Polygon polygon3;
-  visit(reader2, "dummy", polygon3);
-  visit(writer, "polygon", polygon3);
+  visit(reader2, polygon3);
+  visit(writer, polygon3);
   std::cout << std::endl;
 }
--- traverse.h
+++ traverse.h
@@ -9,118 +9,142 @@
 #include <vector>
 
 namespace traverse {
-  // For each visitor you need a type, a visit() function, and one
-  // handle() function per primitive
+
+  /* This is how user-defined structs are described to the system:
+   * 
+   * template<typename Visitor>
+   * void visit(Visitor& visitor, MyUserType& obj) {
+   *   visit_struct(visitor)
+   *      ("x", obj.x)
+   *      ("y", obj.y);
+   * }
+   *
+   * The visit_struct function constructs a local
+   * StructVisitor<Visitor> object, which is destroyed when the
+   * struct's visit() function returns. Each visitor type can
+   * specialize this as needed to keep local fields or do work in the
+   * constructor and destructor.
+   */
   
+  template<typename Visitor>
+  struct StructVisitor {
+    Visitor& visitor;
+    template<typename T>
+    StructVisitor& operator ()(const char* label, T& value) {
+      visit(visitor, value);
+      return *this;
+    }
+  };
+  
+  template<typename Visitor>
+  StructVisitor<Visitor> visit_struct(Visitor& visitor) {
+    return StructVisitor<Visitor>{visitor};
+  }
+
+  /* Each visitor type needs visit() functions for the standard types
+   * it handles (primitives, strings, vectors) and optionally a
+   * StructVisitor to handle the field name/value pairs in a struct.
+   */
+}
+
+
+/* The CoutWriter just writes everything to std::cout */
+
+namespace traverse {
   struct CoutWriter {
   };
 
-  template<typename T>
-  void visit(CoutWriter& writer, const char* label, T& value) {
-    std::cout << "(" << label << ":";
-    handle(writer, value);
-    std::cout << ")";
-  }
-
   template<class T> inline
   typename std::enable_if<std::is_arithmetic<T>::value, void>::type
-  handle(CoutWriter& writer, T& value) {
+  visit(CoutWriter& writer, T& value) {
     std::cout << value;
   }
 
   template<typename Element>
-  void handle(CoutWriter& visitor, std::vector<Element>& vector) {
+  void visit(CoutWriter& visitor, std::vector<Element>& vector) {
+    std::cout << '[';
     for (int i = 0; i < vector.size(); ++i) {
-      // HACK: until I write generic labels
-      char* label = new char[3];
-      sprintf(label, "%d", i);
-      // HACK: ends
-      
-      visit(visitor, label, vector[i]);
+      if (i != 0) std::cout << ", ";
+      visit(visitor, vector[i]);
     }
+    std::cout << ']';
   }
 
-  
-  // Binary serialize
+  template<>
+  struct StructVisitor<CoutWriter> {
+    CoutWriter& visitor;
+    bool first;
+    
+    StructVisitor(CoutWriter& visitor_): visitor(visitor_), first(true) {
+      std::cout << '{';
+    }
+
+    ~StructVisitor() {
+      std::cout << '}';
+    }
+    
+    template<typename T>
+    StructVisitor& operator ()(const char* label, T& value) {
+      if (!first) std::cout << ", ";
+      first = false;
+      std::cout << label << ':';
+      visit(visitor, value);
+      return *this;
+    }
+  };
+}
+
+
+/* The binary serialize/deserialize uses a binary format and stringbufs */
+
+namespace traverse{
 
   struct BinarySerialize {
     std::stringbuf out;
     BinarySerialize(): out(std::ios_base::out) {}
   };
 
-  template<typename T> inline
-  void visit(BinarySerialize& writer, const char* label, T& value) {
-    handle(writer, value);
-  }
-
   template<class T> inline
   typename std::enable_if<std::is_integral<T>::value, void>::type
-  handle(BinarySerialize& writer, T& value) {
+  visit(BinarySerialize& writer, T& value) {
     writer.out.sputn(reinterpret_cast<const char *>(&value), sizeof(T));
   }
 
   template<typename Element>
-  void handle(BinarySerialize& writer, std::vector<Element>& vector) {
+  void visit(BinarySerialize& writer, std::vector<Element>& vector) {
     uint32_t size = vector.size();
     writer.out.sputn(reinterpret_cast<const char*>(&size), sizeof(size));
     for (auto& element : vector) {
-      visit(writer, "#null", element);
+      visit(writer, element);
     }
   }
 
-  // Binary deserialize
 
   struct BinaryDeserialize {
     std::stringbuf in;
     BinaryDeserialize(const std::string& str): in(str, std::ios_base::in) {}
   };
 
-  template<typename T> inline
-  void visit(BinaryDeserialize& reader, const char* label, T& value) {
-    handle(reader, value);
-  }
-
   template<class T> inline
   typename std::enable_if<std::is_integral<T>::value, void>::type
-  handle(BinaryDeserialize& reader, T& value) {
+  visit(BinaryDeserialize& reader, T& value) {
     reader.in.sgetn(reinterpret_cast<char *>(&value), sizeof(T));
   }
 
   template<typename Element>
-  void handle(BinaryDeserialize& reader, std::vector<Element>& vector) {
+  void visit(BinaryDeserialize& reader, std::vector<Element>& vector) {
     uint32_t size = vector.size();
     reader.in.sgetn(reinterpret_cast<char*>(&size), sizeof(size));
     uint32_t i = 0;
     for (; i < size && reader.in.in_avail() > 0; ++i) {
       Element element;
-      visit(reader, "#null", element);
+      visit(reader, element);
       vector.push_back(std::move(element));
     }
     if (i != size) {
       std::cerr << "Warning: expected " << size << " elements in vector but only found " << i << std::endl;
     }
   }
-
-  
-  // Generic traversals
-  template<typename Visitor>
-  struct visit_struct_ {
-    Visitor& visitor;
-    template<typename T>
-    visit_struct_& operator ()(const char* label, T& value) {
-      visit(visitor, label, value);
-      return *this;
-    }
-  };
-  template<typename Visitor>
-  visit_struct_<Visitor> visit_struct(Visitor& visitor) { return visit_struct_<Visitor>{visitor}; }
-
-
-  // TODO: move labels up one level so that vectors don't need labels,
-  // and the begin/end of a struct or vector can be written out (size
-  // for binary, delimiters for json)
-
-  // TODO: vector handling should be per visitor type, not a generic one
 }
 
 
