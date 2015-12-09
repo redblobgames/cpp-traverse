f7507cd0f912dc7a4e80cce14ada54e1087f7f0a
Amit Patel <redblobgames@gmail.com>
2015-12-09 14:46:38 -0800

    Change test format to be a (verbose) unit test
    * Instead of printing the output ad-hoc and expecting the user to scan
      for errors, use TEST_EQ(a,b) that tests that a == b
    * This is less informative from an exploratory point of view but better
      for testing during refactoring

---
+++ run-tests.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+c++ -std=c++14 test.cpp test-link.cpp && ./a.out
+
--- test.cpp
+++ test.cpp
@@ -1,12 +1,19 @@
 // Copyright 2015 Red Blob Games <redblobgames@gmail.com>
 // License: Apache v2.0 <http://www.apache.org/licenses/LICENSE-2.0.html>
 
-/* Compile with:
-   c++ -std=c++14 test.cpp test-link.cpp && ./a.out
- */
-
 #include "traverse.h"
 #include <iostream>
+
+template <typename A, typename B>
+void _test_equal(const A& a, const B& b, const char* file, int line) {
+  if (a != b) {
+    std::cerr << " * FAIL " << file << ':' << line << ": (" << a << ") != (" << b << ")" << std::endl;
+  } else {
+    std::cerr << "   PASS " << file << ':' << line << ": (" << a << ")" << std::endl;
+  }
+}
+#define TEST_EQ(a, b) _test_equal(a, b, __FILE__, __LINE__)
+
 
 #define TEST_JSON 1
 #if TEST_JSON
@@ -29,11 +36,11 @@ private:
 };
 TRAVERSE_STRUCT(LineSegment, FIELD(a) FIELD(b))
 
-// Testing enums
+// Testing C and C++ enums
 enum Color { RED, BLUE };
 enum class Mood { HAPPY, SAD, HULK_SMASH };
 
-// Testing strings, enums, vectors
+// Testing complex structure with enums, strings, vectors
 struct Polygon {
   Color color;
   Mood mood;
@@ -50,92 +57,93 @@ int main() {
   const Point p = {3, 5};
   const LineSegment s{{1, 7}, {13, 19}};
   const Polygon polygon = {BLUE, Mood::HULK_SMASH, "UFO\"1942\"", {{3, 5}, {4, 6}, {5, 7}}};
-  
+
+  std::cout << "__ Basic CoutWriter __" << std::endl;
   {
-    std::cout << "TEST original data: " << polygon << std::endl;
+    std::stringstream out;
+    out << polygon;
+    TEST_EQ(out.str(), "Polygon{color:1, mood:2, name:\"UFO\\\"1942\\\"\", points:[Point{x:3, y:5}, Point{x:4, y:6}, Point{x:5, y:7}]}");
   }
 
-  traverse::BinarySerialize writer2;
+  traverse::BinarySerialize serialize;
+  visit(serialize, polygon);
+
   {
-    std::cout << "TEST serialized to bytes: " << std::endl;
-    visit(writer2, polygon);
-    std::string msg = writer2.out.str();
+    std::cout << "__ Serialize to bytes __ " << std::endl;
+    std::string msg = serialize.out.str();
+    std::stringstream out;
     for (int i = 0; i < msg.size(); i++) {
-      std::cout << int(msg[i]) << ' ';
+      out << int(msg[i]) << ' ';
     }
-    std::cout << std::endl;
-  
-    std::cout << "TEST read back from bytes: " << std::endl;
+    TEST_EQ(out.str(), "1 0 0 0 2 0 0 0 9 0 0 0 85 70 79 34 49 57 52 50 34 3 0 0 0 3 0 0 0 5 0 0 0 4 0 0 0 6 0 0 0 5 0 0 0 7 0 0 0 ");
+
+    std::cout << "__ Deserialize from bytes __ " << std::endl;
+    std::stringstream out1, out2;
     traverse::BinaryDeserialize reader(msg);
     Polygon polygon2;
     visit(reader, polygon2);
-    visit(writer, polygon2);
-    std::cout << std::endl;
-    std::cout << "ERRORS? " << reader.Errors() << std::endl;
+    out1 << polygon;
+    out2 << polygon2;
+    TEST_EQ(out1.str(), out2.str());
+    TEST_EQ(reader.Errors(), "");
   }
 
   {
-    std::cout << "TEST corrupting data: " << std::endl;
-    std::string msg = writer2.out.str();
-    msg[3] = 0x7f;
-    traverse::BinaryDeserialize reader2(msg);
-    Polygon polygon3;
-    visit(reader2, polygon3);
-    visit(writer, polygon3);
-    std::cout << std::endl;
-    std::cout << "ERRORS? " << reader2.Errors() << std::endl;
+    std::cout << "__ Corrupt deserialize __ " << std::endl;
+    std::string msg = serialize.out.str();
+    msg[11] = 0x7f;
+    traverse::BinaryDeserialize reader(msg);
+    Polygon polygon2;
+    visit(reader, polygon2);
+    TEST_EQ(reader.Errors().substr(0, 5), "Error");
   }
 
   {
-    std::cout << "TEST message too short" << std::endl;
-    std::string msg = writer2.out.str();
+    std::cout << "__ Serialized message too short __ " << std::endl;
+    std::string msg = serialize.out.str();
     msg.erase(30);
-    traverse::BinaryDeserialize reader3(msg);
-    Polygon polygon4;
-    visit(reader3, polygon4);
-    visit(writer, polygon4);
-    std::cout << std::endl;
-    std::cout << "ERRORS? " << reader3.Errors() << std::endl;
+    traverse::BinaryDeserialize reader(msg);
+    Polygon polygon2;
+    visit(reader, polygon2);
+    TEST_EQ(reader.Errors().substr(0, 5), "Error");
   }
 
   {
-    std::cout << "TEST message too long" << std::endl;
-    std::string msg = writer2.out.str();
-    msg += "TRASH";
-    traverse::BinaryDeserialize reader3(msg);
-    Polygon polygon4;
-    visit(reader3, polygon4);
-    visit(writer, polygon4);
-    std::cout << std::endl;
-    std::cout << "ERRORS? " << reader3.Errors() << std::endl;
+    std::cout << "__ Serialized message too long __ " << std::endl;
+    std::string msg = serialize.out.str();
+    msg += "12345";
+    traverse::BinaryDeserialize reader(msg);
+    Polygon polygon2;
+    visit(reader, polygon2);
+    TEST_EQ(reader.Errors().substr(0, 5), "Error");
   }
   
 #if TEST_JSON
   {
-    std::cout << "TEST write polygon to json:" << std::endl;
+    std::cout << "__ Serialize to JSON __ " << std::endl;
     picojson::value json1;
     traverse::JsonWriter jsonwriter{json1};
     visit(jsonwriter, polygon);
-    std::cout << json1.serialize(true) << std::endl;
+    TEST_EQ(json1.serialize(), "{\"color\":1,\"mood\":2,\"name\":\"UFO\\\"1942\\\"\",\"points\":[{\"x\":3,\"y\":5},{\"x\":4,\"y\":6},{\"x\":5,\"y\":7}]}");
   }
 
   picojson::value json2;
   {
-    std::cout << "TEST parse json into picojson object:" << std::endl;
+    std::cout << "__ Deserialize from JSON to PicoJSON __ " << std::endl;
     auto err = picojson::parse(json2, "{\"points\":[{\"UNUSED\":0,\"x\":3,\"y\":5},{\"y\":6,\"x\":4},{\"y\":7},{\"x\":\"WRONGTYPE\"}]}");
-    if (!err.empty()) { std::cout << "JSON error " << err << std::endl; }
-    else { std::cout << "(success)" << std::endl; }
+    TEST_EQ(err, "");
   }
 
   {
-    std::cout << "TEST read back from json into polygon:" << std::endl;
+    std::cout << "__ Deserialize JSON to Polygon __ " << std::endl;
     std::stringstream errors;
     traverse::JsonReader jsonreader{json2, errors};
-    Polygon polygon5;
-    visit(jsonreader, polygon5);
-    visit(writer, polygon5);
-    std::cout << std::endl;
-    std::cout << "ERRORS? " << errors.str() << std::endl;
+    Polygon polygon2;
+    visit(jsonreader, polygon2);
+    
+    std::stringstream out;
+    out << polygon2;
+    TEST_EQ(errors.str().substr(0, 7), "Warning");
   }
 #endif
 }
